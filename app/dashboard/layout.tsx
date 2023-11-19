624651f023e5eb8fe3714c288471d80b40aea89e
uyar42 <alimehmet778@gmail.com>
2023-11-19 16:13:07 +0300

    img-alt | bgColor

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className="flex-grow bg-red-400 p-6 md:overflow-y-auto md:p-12">
+      <div className="bg-white-400 flex-grow p-6 md:overflow-y-auto md:p-12">
         {children}
       </div>
     </div>
